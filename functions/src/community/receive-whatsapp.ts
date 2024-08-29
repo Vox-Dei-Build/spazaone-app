b8de7492f68e6bc81a68a2a93c4482745d19c2f2
Tsepo Ntsaba <tsepo.ntsaba.ext@doctorly.de>
2024-08-30 00:24:23 +0200

    Stable POC

--- functions/src/community/receive-whatsapp.ts
+++ functions/src/community/receive-whatsapp.ts
@@ -22,45 +22,29 @@ export const receiveWhatsappWebhook = functions.https.onRequest((req, res) => {
 
   console.log(`Received from ${From}: ${userResponse}`);
 
-  const menuQuickReplies = ["Check Balance", "View Transaction History"];
+  const menuTemplate =
+    "Hello! 👋🏾 Welcome to Pasella! I'm here to help you manage your account, check your balance, view your recent transactions, and more.\n\n🔍 Main Menu:\n1️⃣ Check Balance\n2️⃣ View Transaction History\n\n🚀 Exciting features coming soon, like ordering via WhatsApp, discounts, and promotions!\n\nPlease reply with the number of the option you want to explore.";
+  const shortMenuTemplate =
+    "🔍 Main Menu:\n1️⃣ Check Balance\n2️⃣ View Transaction History\n\nPlease reply with the number of the option you want to explore.";
 
-  const menuTemplate = {
-    body: "Hello! 👋🏾 Welcome to Pasella! I'm here to help you manage your account. What would you like to do?",
-    quickReplies: menuQuickReplies,
-  };
-
-  const shortMenuTemplate = {
-    body: "What would you like to do next?",
-    quickReplies: menuQuickReplies,
-  };
-
-  const handleResponse = (message: any) => {
+  const handleResponse = (message: string) => {
     twiml.message(message);
     res.contentType("text/xml");
     res.send(twiml.toString());
   };
 
-  switch (userResponse?.trim().toLowerCase()) {
-    case "check balance": // Balance Check
+  switch (userResponse?.trim()) {
+    case "1": // Balance Check
       fetchUserBalance(From)
-        .then((balance) =>
-          handleResponse({
-            body: `${balance}\n\nWhat would you like to do next?`,
-            quickReplies: menuQuickReplies,
-          }),
-        )
-        .catch(() =>
-          handleResponse({
-            body: `${history}\n\nWhat would you like to do next?`,
-            quickReplies: menuQuickReplies,
-          }),
-        );
+        .then((balance) => handleResponse(`${balance}\n\n${shortMenuTemplate}`))
+        .catch(() => handleResponse("⚠️ Failed to fetch balance."));
       break;
-    case "view transaction history": // Transactions History
+    case "2": // Transactions History
       fetchTransactionHistory(From)
         .then((history) => handleResponse(`${history}\n\n${shortMenuTemplate}`))
         .catch(() => handleResponse("⚠️ Failed to fetch transactions."));
       break;
+    case "0":
     default:
       handleResponse(menuTemplate);
       break;
