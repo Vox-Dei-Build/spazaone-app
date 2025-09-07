ce4fff8060a7e6434db5358fa489b5602297ea77
TsepoDelta <tsepo.ntsaba@thedelta.io>
2025-09-07 23:53:38 +0200

    chore: latest progress on ios version

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import FirebaseMessaging
 import flutter_branch_sdk
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
